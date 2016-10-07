0804b76ddf829446e9b48b254e237965c8934b32
JeroenCompagnie <jeroen.compagnie@gmail.com>
2016-10-07 15:20:30 +0200

    Added class with a bug

---
+++ classWithBug.java
@@ -0,0 +1,20 @@
+public class ClassWithBug{
+
+	private String string1;
+
+	public ClassWithBug(String string) {
+		string1 = string;
+	}
+
+	private String getString1() {
+		return string1;
+	}
+	
+	public static void main(String [ ] args){
+		ClassWithBug obj = new ClassWithBug("Test");
+		System.out.println("This is the string " + obj.getString1();
+	}
+
+	
+	
+}
